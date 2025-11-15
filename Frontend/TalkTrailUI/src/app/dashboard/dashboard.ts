8d41ca78603c5b1a151867e5083ce729f6d072f8
Vivek Kushwaha <vivekkushwaha2727@gmail.com>
2025-11-15 21:46:26 +0530

    Created the dashboard component.

---
+++ Frontend/TalkTrailUI/src/app/dashboard/dashboard.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [CommonModule, RouterLink],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.scss'
+})
+export class Dashboard {
+
+}
